71ff2495ab20dd4d8196d1f26335e6a75feede26
machty <machty@gmail.com>
2018-04-05 22:56:47 -0400

    add test for nested state

--- tests/unit/dsl-test.ts
+++ tests/unit/dsl-test.ts
@@ -8,25 +8,25 @@ module('Unit - DSL test', {
   },
 });
 
-const map = createMap(() => [
-  route('foo', { path: 'foo/:foo_id' }, () => [
-    route('foochild', { path: 'foochild2' }),
-    state('admin', (adminState) => [
-      route('posts'),
-      when({ foo: 123 }, () => [
-        route('comments')
-      ]),
-      when({ other: 123 }, () => [
-        route('comments')
-      ]),
-    ]),
-    state('my-service')
-  ]),
-  route('bar'),
-]);
-
 test('recognizing', function (assert) {
   assert.expect(7);
+
+  let map = createMap(() => [
+    route('foo', { path: 'foo/:foo_id' }, () => [
+      route('foochild', { path: 'foochild2' }),
+      state('admin', (adminState) => [
+        route('posts'),
+        when({ foo: 123 }, () => [
+          route('comments')
+        ]),
+        when({ other: 123 }, () => [
+          route('comments')
+        ]),
+      ]),
+      state('my-service')
+    ]),
+    route('bar'),
+  ]);
 
   let result;
   result = map.recognize('foo/123/foochild2');
@@ -42,6 +42,23 @@ test('recognizing', function (assert) {
   assert.equal(results.length, 2);
   assert.deepEqual(mapResult(results[0]), [ "foo_.0", "admin_.0.1", "when_.0.1.1", "comments_.0.1.1.0" ]);
   assert.deepEqual(mapResult(results[1]), [ "foo_.0", "admin_.0.1", "when_.0.1.2", "comments_.0.1.2.0" ]);
+});
+
+test('nested state paradigm', function (assert) {
+  assert.expect(2);
+
+  let map = createMap(() => [
+    state('s1', () => [
+      state('s2', () => [
+        route('bar')
+      ]),
+    ]),
+  ]);
+
+  let results;
+  results = map.recognizeAll('bar');
+  assert.equal(results.length, 1);
+  assert.deepEqual(mapResult(results[0]), [ "s1_.0", "s2_.0.0", "bar_.0.0.0" ]);
 });
 
 function mapResult(result) {
