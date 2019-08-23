98fc678751b0dc68e38d6dce40cde5ec7b2c2698
machty <machty@gmail.com>
2019-08-23 07:08:55 -0400

    delete old stuff

--- tests/dummy/app/routes/user-absent.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-import { guidFor } from '@ember/object/internals';
-import { computed } from '@ember/object';
-
-export default Route.extend({
-  name: computed(function() {
-    return `user-absent-${guidFor(this)}`;
-  }),
-});
