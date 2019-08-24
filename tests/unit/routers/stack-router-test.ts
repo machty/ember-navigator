8f490ba0b3c0eb1d288e2982b92b0349c2089f2b
machty <machty@gmail.com>
2019-08-24 17:03:57 -0400

    cleanup

--- tests/unit/routers/stack-router-test.ts
+++ tests/unit/routers/stack-router-test.ts
@@ -4,14 +4,9 @@ import { route, stackNavigator } from 'ember-constraint-router/map';
 module('Unit - StackRouter test');
 
 test("it provides an overridable componentName", function (assert) {
-  let stackRouter1 = stackNavigator('root', [
-    route('foo'),
-  ]);
-
-  let stackRouter2 = stackNavigator('root', [
-    route('foo'),
-  ], { componentName: "x-foo" });
-
+  let children = [ route('foo') ];
+  let stackRouter1 = stackNavigator('root', children);
+  let stackRouter2 = stackNavigator('root', children, { componentName: "x-foo" });
   assert.equal(stackRouter1.componentName, "ecr-stack");
   assert.equal(stackRouter2.componentName, "x-foo");
 });
