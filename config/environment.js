5c1916b8aac35f05259df23062cb0cc65aa6878d
machty <machty@gmail.com>
2019-08-25 02:18:43 -0400

    try things

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,11 @@
 /* eslint-env node */
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
-  return { };
+module.exports = function(environment /* , appConfig */) {
+  let ENV = {}
+  if (environment === 'production') {
+    ENV.rootURL = '/ember-constraint-router';
+    ENV.locationType = 'hash';
+  }
+  return ENV;
 };
