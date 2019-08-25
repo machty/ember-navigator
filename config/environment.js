e8866d6806d8c7db545526397989005d3b29398b
machty <machty@gmail.com>
2019-08-25 02:30:12 -0400

    deploy vars

--- config/environment.js
+++ config/environment.js
@@ -1,11 +1,5 @@
 /* eslint-env node */
 'use strict';
 
-module.exports = function(environment /* , appConfig */) {
-  let ENV = {}
-  if (environment === 'production') {
-    ENV.rootURL = '/ember-constraint-router';
-    ENV.locationType = 'hash';
-  }
-  return ENV;
+module.exports = function(/* environment, appConfig */) {
 };
