70c932a1cc362566f2e3c9ee3a50bca4499b9d35
machty <machty@gmail.com>
2019-08-25 01:19:43 -0400

    move tweet Route into config

--- tests/dummy/app/components/frame-tweet.ts
+++ tests/dummy/app/components/frame-tweet.ts
@@ -4,9 +4,10 @@ import layout from '../templates/components/frame-tweet';
 
 export default class FrameTweet extends Component {
   layout = layout;
-  provided: { tweet: any; }
 
-  get tweet() {
-    return this.provided.tweet;
+  static Config = {
+    header: {
+      title: "Tweet"
+    }
   }
 }
