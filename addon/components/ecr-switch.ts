81894d9b975340590250c7f4333435057016fd5c
machty <machty@gmail.com>
2022-07-15 10:02:56 -0400

    glimmerize ecr-switch

--- addon/components/ecr-switch.ts
+++ addon/components/ecr-switch.ts
@@ -1,20 +1,18 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { computed } from '@ember/object';
 import { RouterState } from 'ember-navigator/-private/routeable';
 import { MountedNode } from 'ember-navigator/-private/mounted-node';
 
-export default class EcrSwitch extends Component.extend({
-  tagName: null,
-  classNames: 'ecr-switch',
+export default class EcrSwitch extends Component {
+  state?: RouterState;
 
-  currentNodes: computed('route.node', function() {
-    let node = this.node as MountedNode;
+  @computed('args.route.node')
+  get currentNodes() {
+    let node = this.args.node as MountedNode;
     let routerState = node.routeableState as RouterState;
     let activeChild = routerState.routes[routerState.index];
     let activeChildNode = node.childNodes[activeChild.key];
     return [activeChildNode];
-  }),
-}) {
-  state?: RouterState;
-};
+  }
+}
 
